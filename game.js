24f2fbf34867e7c2553c30342bd0b1156c25ba4d
Mitsrael Souza <mitsrael9@gmail.com>
2021-05-21 17:29:51 -0300

    First commit

---
+++ game.js
@@ -0,0 +1,100 @@
+const player1 = "X"
+const player2 = "O"
+var playerTurn = player1
+var gameOver = false
+var spaces = document.getElementsByClassName("espaco")
+
+updatedisplay();
+startSpaces();
+
+function updatedisplay() {
+    if (gameOver) { return }
+
+    if (playerTurn == player1) {
+        var player = document.querySelectorAll("div#mostrador img")[0]
+        player.setAttribute("src", "imagens/x.png")
+    } else {
+        var player = document.querySelectorAll("div#mostrador img")[0]
+        player.setAttribute("src", "imagens/circle.png")
+    }
+};
+
+function startSpaces() {
+    for (var i = 0; i < spaces.length; i++) {
+        spaces[i].addEventListener("click", function() {
+            if (gameOver) { return; }
+
+            if (this.getElementsByTagName("img").length == 0) {
+                if (playerTurn == player1) {
+                    this.innerHTML = "<img src='./imagens/x.png'>"
+                    this.setAttribute("jogada", player1)
+                    playerTurn = player2
+
+                } else {
+                    this.innerHTML = "<img src='./imagens/circle.png'>"
+                    this.setAttribute("jogada", player2)
+                    playerTurn = player1
+                }
+
+                updatedisplay();
+                checkWinner();
+            }
+        })
+    }
+};
+
+async function checkWinner() {
+    var a1 = document.getElementById("a1").getAttribute("jogada")
+    var a2 = document.getElementById("a2").getAttribute("jogada")
+    var a3 = document.getElementById("a3").getAttribute("jogada")
+
+    var b1 = document.getElementById("b1").getAttribute("jogada")
+    var b2 = document.getElementById("b2").getAttribute("jogada")
+    var b3 = document.getElementById("b3").getAttribute("jogada")
+
+    var c1 = document.getElementById("c1").getAttribute("jogada")
+    var c2 = document.getElementById("c2").getAttribute("jogada")
+    var c3 = document.getElementById("c3").getAttribute("jogada")
+    
+    let winner = "";
+
+    if (((a1 == b1 && a1 == c1) || (a1 == a2 && a1 == a3) || (a1 == b2 && a1 == c3)) && a1 != "") {
+        winner = a1
+    } else if (((b2 == b1 && b2 == b3) || (b2 == a2 && b2 == c2) || (b2 == a3 && b2 == c1))  && b2 != ""){
+        winner = b2
+    } else if (((c3 == c2 & c3 == c1) || (c3 == a3 && c3 == b3)) && c3 != "") {
+        winner = c3
+    } else {
+        if (a1 != "" && a2 != "" && a3 != "" && b1 != "" &&  b2 != "" && b3 != "" && c1 != "" && c2 != "" && c3 != ""){
+            winner = "Ninguém ganhou dessa vez"
+        }
+    }
+   
+    if (winner != "") {
+        gameOver = true
+
+        await sleep(50)
+
+        alert(`O vencedor foi o: '${winner}'!`)
+    }
+}
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+let restart = document.getElementById("restart")
+
+restart.addEventListener("click", function (){
+    for(let i = 0; i < spaces.length; i++){
+        spaces[i].innerHTML = "";
+        spaces[i].setAttribute("jogada", '');
+
+        playerTurn = player1
+
+        updatedisplay()
+    }
+
+    gameOver = false;
+
+})
